3ed6014e0ea4cc8a77e9d60c21b9977c370e4b01
Iclipsee <iclipskii@gmail.com>
2019-09-18 22:36:40 +0200

    Initial Commit

---
+++ src/net/projectx/api/main/Main.java
@@ -0,0 +1,24 @@
+package net.projectx.api.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * ~Yannick on 18.09.2019 at 22:15 o´ clock
+ */
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
